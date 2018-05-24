6c052fa969c4bc894bd35be14292f96eb67441b3
Amalie S <amaliesmidth@gmail.com>
2018-05-24 14:53:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ describe('yourAnimal()', function() {
   })
 
 function yourAnimal() {
-
+  // How can we make sure that this function
+  // and the above function both pass?
+  // P.S.: You can't just hard-code 'cat' below
   return animal
 }
 
